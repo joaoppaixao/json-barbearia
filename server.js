8cca7122d4878b33deddff510cedeaa6101d4854
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2025-07-03 21:01:19 -0300

    atualizei o server.js

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// Liberar CORS para permitir que o frontend acesse
 server.use(middlewares);
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Permite qualquer origem
+  res.header('Access-Control-Allow-Headers', '*'); // Permite todos os headers
+  next();
+});
+
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
